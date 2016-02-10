0c572c6e852eeb992e06bd2117f039eaf9255473
Sidharth <sidharth.mm@gmail.com>
2016-02-10 11:51:38 -0500

    From Intellij

--- insense-core/src/main/java/com/ensense/insense/core/Test.java
+++ insense-core/src/main/java/com/ensense/insense/core/Test.java
@@ -9,5 +9,6 @@ public class Test {
     public static void main(String[] args){
         System.out.println("Hello");
         System.out.println("Hello world");
+        System.out.println("Hello world.");
     }
 }
